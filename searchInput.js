df47618fd05427cb7aca4d957b678174aee2fcec
Aiman Shahpuri <125454433+aimanshahpuri@users.noreply.github.com>
2024-04-15 16:34:53 +0530

    Delete searchInput.js

--- searchInput.js
+++
@@ -1,9 +0,0 @@
-    //search funtion
-    function search() {
-        let searchWord = $("#searchInput").val();
-        let link = `https://www.instagram.com/explore/tags/${searchWord}`
-        if (window.event.keyCode == 13) {
-            location.href = link;
-        }
-
-    }
