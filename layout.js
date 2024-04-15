82d92156a05c2b6dc09da851293a3c93958ac541
Aiman Shahpuri <125454433+aimanshahpuri@users.noreply.github.com>
2024-04-15 16:34:07 +0530

    Delete layout.js

--- layout.js
+++
@@ -1,8 +0,0 @@
-    
-//Codes included inside $( document ).ready() will only run once the page
-   $(document).ready(function () {
-        renderStory()
-        captionFirst()
-        captionSecond()
-        captionThird()
-    });
